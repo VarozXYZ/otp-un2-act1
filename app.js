5cd5609f6b936c9550777ce8923bb3c64786c594
VarozXYZ <alvaropiro@gmail.com>
2025-11-19 21:31:43 +0100

    put modify endpoint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-const db = [
+let db = [
     { id: 1, name: "Cloud Strife", job: "Soldier", weapon: "Buster sword", level: 25 },
     { id: 2, name: "Tifa Lockhart", job: "Fighter", weapon: "Leather gloves", level: 22 },
     { id: 3, name: "Aerith Gainsborough", job: "Mage", weapon: "Magic staff", level: 20 }
@@ -40,7 +40,33 @@ app.post("/characters", (req, res) => {
     }
     
     db.push(newChar);
-    res.send("Character added successfully");
+    res.status(201).send("Character added successfully");
+})
+
+app.put("/characters/:id", (req, res) => {
+    const newData = req.body;
+    const idToModify = req.params.id;
+    if (!newData) {
+        return res.status(400).send("Empty character data");
+    }
+    const oldChar = db.find(c => c.id == idToModify);
+    if (!oldChar) {
+        return res.status(404).send("Character does not exist");
+    }
+    const newDb = db.filter(c => c.id != idToModify);
+    for (const character of newDb) {
+        if (newData.name == character.name) {
+            return res.status(400).send("Repeated character name");
+        } else if (newData.id == character.id) {
+            return res.status(400).send("Repeated character ID");
+        }
+    }
+    if (newData.level < 1 || newData.level > 100) {
+        return res.status(400).send("Level must be between 1 and 99");
+    }
+    db = newDb;
+    db.push(newData);
+    res.sendStatus(204);
 })
 
 app.listen(8080, () => {
