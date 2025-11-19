b9b0f21b98dda2803613e2437bebfa9b7650493b
VarozXYZ <alvaropiro@gmail.com>
2025-11-19 20:41:13 +0100

    post character endpoint

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ const db = [
 ]
 
 const app = express();
+app.use(express.json());
 
 app.get("/characters", (req, res) => {
     res.send(db);
 })
 
 app.get("/characters/:id", (req, res) => {
-    console.log(req.params.id)
-
     db.forEach(character => {
-        console.log(character)
         if (character.id == req.params.id) {
             res.send(character)
         }
@@ -24,6 +22,27 @@ app.get("/characters/:id", (req, res) => {
     res.status(404).send("Character ID not found on database");
 })
 
+app.post("/characters", (req, res) => {
+    const newChar = req.body;
+    if (!newChar) {
+        res.status(400).send("Empty character data")
+    }
+    let unique = true;
+    db.forEach(character => {
+        if (newChar.id == character.id || newChar.name == character.name) {
+            unique = false;
+        }
+    })
+    if (!unique) {
+        res.status(400).send("Repeated character ID or name")
+    } else if (newChar.level < 1 || newChar.level > 100) {
+        res.status(400).send("Level must be between 1 and 99")
+    } else {
+        db.push(newChar);
+        res.send("Character added successfully");
+    }
+})
+
 app.listen(8080, () => {
-    console.log("Final Fantasy VII Remake character manager started. (Port: 8080)")
+    console.log("Final Fantasy VII Remake character manager started (Port: 8080)")
 })
